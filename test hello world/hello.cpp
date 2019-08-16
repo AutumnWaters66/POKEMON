6a75431d9ebc9128257f5795ed0de65fa423f97d
310614 <569831010@qq.com>
2019-08-16 11:46:10 +0800

    add fight

--- test hello world/hello.cpp
+++ test hello world/hello.cpp
@@ -27,6 +27,8 @@ int pokemon_experiece = 0;                //定义我方精灵经验
 int exercise_Width = 0;                         //我方经验槽长度
 int canvas[1652][1416] = { 0 };               //定义画布坐标 使每个像素点坐标为0
 
+int jump2 = 5;                //看起来精灵在跳动
+
 							  //NPC的位置坐标
 int doctor_tong_x, doctor_tong_y;
 int NPC_1_x, NPC_1_y;
@@ -1016,6 +1018,62 @@ void operate()
 	}
 }
 
+//导入敌方精灵图片
+void load_PK_picture(pokemon* PK)
+{
+	char ch_path1[60] = "资源文件夹\\其它\\";                                     //路径前端
+	char ch_path2[60] = ".jpg";                                            //路径后端
+	char ch_path3[60] = "bk.jpg";
+	char ch_path4[60] = "资源文件夹\\其它\\";
+	strcat_s(ch_path1, PK->name);          //将路径名称链接起来
+	strcat_s(ch_path1, ch_path2);
+	strcat_s(ch_path4, PK->name);
+	strcat_s(ch_path4, ch_path3);
+	TCHAR p1[60];
+	TCHAR p2[60];
+	CharToTchar(ch_path1, p1);
+	CharToTchar(ch_path4, p2);
+	//将路径名字改成TCHAR类型来调用//
+	static int jumpy_1 = 90;
+	if (jump2 == 5)
+	{
+		jumpy_1 = 90;
+	}
+
+	if (jump2 == 10)
+	{
+		jump2 = 0;
+		jumpy_1 = 95;
+	}
+	if (jump2 == 15)
+	{
+		jump2 = 0;
+		jumpy_1 = 100;
+	}
+	if (jump2 == 20)
+	{
+		jump2 = 0;
+		jumpy_1 = 80;
+	}
+	jump2++;
+	//显示敌方精灵//
+	IMAGE im1, im2;
+	loadimage(&im1, p1);           //以TCHAR形式导入图片
+	loadimage(&im2, p2);
+	putimage(400, jumpy_1, &im2, NOTSRCERASE);
+	putimage(400, jumpy_1, &im1, SRCINVERT);
+}
+
+//导入敌方技能
+void load_PK_skill(pokemon* PK, int PK_bleed, int full_bleed)
+{
+	void(*funcPK)();  //定义一个函数指针，空函数
+	if (PK_bleed >= full_bleed / 2.0)
+		funcPK = PK->skill1;  //把函数名调用出来
+	else
+		funcPK = PK->skill2;
+	(*funcPK)();
+}
 //界面切换动画
 void interface_change_animation()
 {
@@ -1037,6 +1095,459 @@ void interface_change_animation()
 			i++;
 	}
 	//到这里动画结束
+}
+
+//战斗显示函数
+void fight_show(pokemon* PK_enemy, pokemon* PK_own)
+{
+	mciSendString(_T("close bkmusic"), NULL, 0, NULL);
+
+	int x = 30;
+	int y = 365;                                       //技能选择图标坐标
+
+	int choice_x = 370;
+	int choice_y = 365;								//开始界面选择
+	int choose = 1;                                  //选择战斗or逃跑的判断
+
+	int jumpy = 150;                               //己方精灵坐标
+	int jump = 5;									  //精灵循环跳动变量
+	char input;										  //输入判断变量
+
+	int fullbleed = PK_own->bleed;				//我方满血血量
+	int fullbleed2 = PK_enemy->bleed;           //敌方满血血量
+
+	int bleed_Width2 = 125;
+	int bleed_Width1 = 180;					    //我方血条长度
+
+	int fullexercise_Width = 240;                  //满经验槽长度
+
+	int turn = 1;
+
+	mciSendString(_T("play fightmusic repeat"), NULL, 0, NULL);
+
+	while (1)
+	{
+		IMAGE img1, imbkk;
+		loadimage(&img1, _T("资源文件夹\\其它\\战斗背景1.jpg"));
+		putimage(0, 0, &img1);
+		//我方血条框
+		setlinestyle(PS_SOLID, 3);						   //边框宽度
+		setlinecolor(BLACK);							      //边框颜色
+		setfillcolor(RGB(255, 245, 220));               //填充颜色
+		fillroundrect(25, 50, 220, 130, 10, 10);      //边框大小
+													  //敌方血条框
+		setlinestyle(PS_SOLID, 3);                        //边框宽度
+		setlinecolor(BLACK);                              //边框颜色
+		setfillcolor(RGB(255, 245, 220));             //填充颜色
+		fillroundrect(330, 220, 580, 310, 10, 10); //边框大小
+
+												   //最下方技能框
+		setfillcolor(BLACK);
+		solidrectangle(0, 335, 640, 480);            //黑色背景   
+
+		setfillcolor(LIGHTGRAY);
+		solidroundrect(5, 340, 340, 475, 10, 10);
+		setfillcolor(WHITE);
+		solidroundrect(15, 350, 330, 465, 10, 10);//左边技能框
+
+		setfillcolor(LIGHTGRAY);
+		solidroundrect(345, 340, 635, 475, 10, 10);
+		setfillcolor(WHITE);
+		solidroundrect(355, 350, 625, 465, 10, 10);//右边技能框
+
+
+		if (jump == 5)
+		{
+			jumpy = 150;                                //己方精灵循环上下跳动
+		}
+
+		if (jump == 10)
+		{
+			jumpy = 155;
+			jump = 0;
+		}
+		jump++;
+
+
+		//皮卡丘
+		IMAGE im1, im2;
+		loadimage(&im1, _T("资源文件夹\\其它\\皮卡丘.jpg"));
+		loadimage(&im2, _T("资源文件夹\\其它\\皮卡丘bk.jpg"));
+		putimage(65, jumpy, &im2, NOTSRCERASE);
+		putimage(65, jumpy, &im1, SRCINVERT);
+
+		//我方精灵名字
+		TCHAR Name[5];
+		settextcolor(BLUE);
+		LOGFONT f;
+		gettextstyle(&f);                     // 获取当前字体设置
+		f.lfHeight = 20;                      // 设置字体高度为 48
+		wcscpy_s(f.lfFaceName, _T("黑体"));    // 设置字体为“黑体"
+		f.lfQuality = ANTIALIASED_QUALITY;    // 设置输出效果为抗锯齿  
+		settextstyle(&f);                     // 设置字体样式
+		CharToTchar(PK_own->name, Name);
+		outtextxy(370, 230, Name);
+
+		//我方等级
+		TCHAR Lv[5];
+		_stprintf(Lv, _T("%d"), PK_own->level);
+		settextcolor(BLACK);
+		outtextxy(500, 230, Lv);
+		outtextxy(460, 230, _T("Lv:"));
+
+		//我方精灵血量显示
+		outtextxy(490, 280, _T("/"));
+		TCHAR HP2[5];
+		_stprintf(HP2, _T("%d"), PK_own->bleed);
+		outtextxy(430, 280, HP2);
+		TCHAR HP2_1[5];
+		_stprintf(HP2_1, _T("%d"), fullbleed);
+		outtextxy(530, 280, HP2_1);
+
+		//我方血条
+		setlinestyle(PS_SOLID, 2);
+		setlinecolor(LIGHTGRAY);
+		setfillcolor(WHITE);
+		fillroundrect(385, 260, 570, 270, 10, 10);
+
+		setfillcolor(GREEN);
+		solidroundrect(568 - bleed_Width1, 262, 568, 267, 10, 10);
+
+		//我方经验槽
+		setlinestyle(PS_SOLID, 2);
+		setlinecolor(LIGHTGRAY);
+		setfillcolor(WHITE);
+		fillroundrect(335, 300, 575, 305, 10, 10);
+
+		setfillcolor(GREEN);
+		solidroundrect(337, 302, 337 + exercise_Width, 302, 10, 10);
+
+		//敌方精灵名字
+		TCHAR Name_2[5];
+		settextcolor(RED);
+		CharToTchar(PK_enemy->name, Name_2);
+		outtextxy(40, 70, Name_2);
+
+		//敌方等级
+		TCHAR Lv_2[5];
+		_stprintf(Lv_2, _T("%d"), PK_enemy->level);
+		settextcolor(BLACK);
+		outtextxy(160, 70, Lv_2);
+		outtextxy(120, 70, _T("Lv:"));
+
+		//敌方技能名字
+		TCHAR skill_name1[5];
+		CharToTchar(PK_enemy->skill1_name, skill_name1);
+		TCHAR skill_name2[5];
+		CharToTchar(PK_enemy->skill2_name, skill_name2);
+
+
+		//血条
+		setlinestyle(PS_SOLID, 2);
+		setlinecolor(LIGHTGRAY);
+		setfillcolor(WHITE);
+		fillroundrect(80, 100, 210, 110, 10, 10);
+
+		setfillcolor(GREEN);
+		solidroundrect(208 - bleed_Width2, 102, 208, 107, 10, 10);
+
+		getimage(&imbkk, 350, 0, 300, 480);   //
+		load_PK_picture(PK_enemy);     //显示敌方精灵
+
+
+									   //选择战斗or逃跑
+		if (choose == 1)
+		{
+			setfillcolor(BLACK);
+			POINT pts[] = { { choice_x, choice_y },{ choice_x,choice_y + 20 },{ choice_x + 15, choice_y + 10 } };
+			solidpolygon(pts, 3);
+
+			char input;
+			if (_kbhit())
+			{
+				input = _getch();
+				if ((input == 'd' || input == 'D') && choice_x < 370 + 130)
+					choice_x += 130;
+				if ((input == 'a' || input == 'A') && choice_x > 370)
+					choice_x -= 130;
+				if ((input == 's' || input == 'S') && choice_y < 365 + 50)
+					choice_y += 50;
+				if ((input == 'w' || input == 'W') && choice_y > 365)
+					choice_y -= 50;
+				if ((input == 'z' || input == 'Z') && choice_x == 370 && choice_y == 365)
+					choose = 0;
+				if ((input == 'z' || input == 'Z') && choice_x == 370 + 130 && choice_y == 365)
+				{
+					PK_enemy->judge = 1;
+					mciSendString(_T("close fightmusic"), NULL, 0, NULL);
+					startup_music();
+					mciSendString(_T("play bkmusic"), NULL, 0, NULL);
+					break;
+				}
+			}
+
+			LOGFONT f;
+			gettextstyle(&f);                     // 获取当前字体设置
+			f.lfHeight = 20;                      // 设置字体高度
+			wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+			setbkmode(TRANSPARENT);
+			settextcolor(BLACK);
+			settextstyle(&f);                     // 设置字体样式
+			outtextxy(370 + 20, 365, _T("战斗"));
+			outtextxy(500 + 20, 365, _T("逃跑"));
+
+			FlushBatchDraw();
+		}
+
+		//我方技能
+		if (choose == 0)
+		{
+			//技能选择图标
+			setfillcolor(BLACK);
+			POINT pts[] = { { x, y },{ x, y + 20 },{ x + 15, y + 10 } };
+			solidpolygon(pts, 3);
+
+			LOGFONT f;
+			gettextstyle(&f);                     // 获取当前字体设置
+			f.lfHeight = 20;                      // 设置字体高度
+			wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+			setbkmode(TRANSPARENT);
+			settextcolor(BLACK);
+			settextstyle(&f);                      // 设置字体样式
+			outtextxy(30 + 20, 365, _T("十万伏特"));
+			outtextxy(190 + 20, 365, _T("尖叫"));
+			outtextxy(30 + 20, 365 + 60, _T("电磁波"));
+			outtextxy(190 + 20, 365 + 60, _T("打雷"));
+
+			//判断敌方精灵有没有死亡//
+			if (PK_enemy->bleed <= 0)
+			{
+				LOGFONT f;
+				gettextstyle(&f);                     // 获取当前字体设置
+				f.lfHeight = 100;                     // 设置字体高度
+				wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+				setbkmode(TRANSPARENT);
+				settextcolor(RED);
+				settextstyle(&f);                     // 设置字体样式
+				outtextxy(window_width / 2 - 150, window_height / 2 - 100, _T("victory"));    //显示游戏结果//
+				FlushBatchDraw();
+				PK_enemy->judge = 1;
+				Sleep(2000);
+				pokemon_experiece += 50 + PK_enemy->number * 10;   //我方精灵经验增加
+				if (pokemon_experiece >= 50 * (PK_own->level + 1) * (PK_own->level + 1) - 350 * (PK_own->level + 1) + 500)
+				{
+					PK_own->level++;
+					exercise_Width = (pokemon_experiece - 50 * PK_own->level * PK_own->level + 350 * PK_own->level - 500) * 1.0 / (200 + (PK_own->level - 5) * 100) * fullexercise_Width;   //经验槽宽度
+
+				}
+				else
+				{
+					if (PK_own->level == 5)
+						exercise_Width = pokemon_experiece * 1.0 / 200 * fullexercise_Width;
+					else if (PK_own->level > 5)
+						exercise_Width = (pokemon_experiece - 50 * PK_own->level * PK_own->level + 350 * PK_own->level - 500) * 1.0 / (200 + (PK_own->level - 5) * 100) * fullexercise_Width;   //经验槽宽度
+				}
+				mciSendString(_T("close fightmusic"), NULL, 0, NULL);
+				startup_music();
+				mciSendString(_T("play bkmusic"), NULL, 0, NULL);
+				break;
+
+
+			}
+			//判断己方精灵有没有死亡//
+			if (PK_own->bleed <= 0)
+			{
+				LOGFONT f;
+				gettextstyle(&f);                     // 获取当前字体设置
+				f.lfHeight = 100;                     // 设置字体高度
+				wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+				setbkmode(TRANSPARENT);
+				settextcolor(RED);
+				settextstyle(&f);                     // 设置字体样式
+				outtextxy(window_width / 2 - 150, window_height / 2 - 100, _T("defeat"));    //显示游戏结果//
+				FlushBatchDraw();
+				PK_enemy->judge = 1;
+				Sleep(2000);
+				mciSendString(_T("close fightmusic"), NULL, 0, NULL);
+				startup_music();
+				mciSendString(_T("play bkmusic"), NULL, 0, NULL);
+				break;
+			}
+
+			if (turn == 0)  //敌方技能
+			{
+
+				LOGFONT f;
+				gettextstyle(&f);                     // 获取当前字体设置
+				f.lfHeight = 25;                      // 设置字体高度
+				wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+				setbkmode(TRANSPARENT);
+				settextcolor(RED);
+				settextstyle(&f);                      // 设置字体样式
+				outtextxy(370, 360, Name_2);
+				outtextxy(480, 360, _T("!"));
+				outtextxy(370, 400, _T("使用了"));
+				if (PK_enemy->bleed >= fullbleed2 / 2.0)
+					outtextxy(450, 400, skill_name1);
+				else
+					outtextxy(450, 400, skill_name2);
+				FlushBatchDraw();							  //结束批量绘制
+				Sleep(1000);
+				load_PK_skill(PK_enemy, PK_enemy->bleed, fullbleed2);
+				PK_own->bleed = PK_own->bleed - ((PK_enemy->level * 0.4 + 2) * PK_enemy->skill_1 * PK_enemy->attack / (PK_own->defence * 50.0) + 2);  //伤害值计算//
+				if (PK_own->bleed >= 0)
+				{
+					bleed_Width1 = PK_own->bleed * 1.0 / fullbleed * 180;
+				}
+				else
+				{
+					bleed_Width1 = 0 / fullbleed2 * 180;
+				}
+				turn = 1;
+			}
+			//control控制部分
+			if (turn == 1)
+			{
+				if (_kbhit())										 //操作判断
+				{
+					input = _getch();
+					if ((input == 'd' || input == 'D') && x < 190)
+						x += 160;
+					if ((input == 'a' || input == 'A') && x > 30)
+						x -= 160;
+					if ((input == 's' || input == 'S') && y < 365 + 60)
+						y += 60;
+					if ((input == 'w' || input == 'W') && y > 365)
+						y -= 60;
+					if (input == 'z' || input == 'Z')
+					{
+						if (x == 30 && y == 365)
+						{
+							LOGFONT f;
+							gettextstyle(&f);                     // 获取当前字体设置
+							f.lfHeight = 25;                      // 设置字体高度
+							wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+							setbkmode(TRANSPARENT);
+							settextcolor(RED);
+							settextstyle(&f);                      // 设置字体样式
+							outtextxy(370, 360, Name);
+							outtextxy(480, 360, _T("!"));
+							outtextxy(380, 400, _T("使用十万伏特"));
+							FlushBatchDraw();							  //结束批量绘制
+							Sleep(500);
+							lighting();
+							PK_enemy->bleed = PK_enemy->bleed - ((PK_own->level * 0.4 + 2) * PK_own->skill_1 * PK_own->attack / (PK_enemy->defence * 50.0) + 2);  //伤害值计算//
+						}
+						if (x == 190 && y == 365)
+						{
+							LOGFONT f;
+							gettextstyle(&f);                     // 获取当前字体设置
+							f.lfHeight = 25;                      // 设置字体高度
+							wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+							setbkmode(TRANSPARENT);
+							settextcolor(RED);
+							settextstyle(&f);                      // 设置字体样式
+							outtextxy(370, 360, Name);
+							outtextxy(480, 360, _T("!"));
+							outtextxy(380, 400, _T("尖叫"));
+							FlushBatchDraw();							  //结束批量绘制
+							Sleep(500);
+							scream();
+							PK_enemy->bleed = PK_enemy->bleed - ((PK_own->level * 0.4 + 2) * PK_own->skill_2 * PK_own->attack / (PK_enemy->defence * 50.0) + 2);  //伤害值计算//
+						}
+						if (x == 30 && y == 365 + 60)
+						{
+							LOGFONT f;
+							gettextstyle(&f);                     // 获取当前字体设置
+							f.lfHeight = 25;                      // 设置字体高度
+							wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+							setbkmode(TRANSPARENT);
+							settextcolor(RED);
+							settextstyle(&f);                      // 设置字体样式
+							outtextxy(370, 360, Name);
+							outtextxy(480, 360, _T("!"));
+							outtextxy(380, 400, _T("电磁波"));
+							FlushBatchDraw();							  //结束批量绘制
+							Sleep(500);
+							flash();
+							PK_enemy->bleed = PK_enemy->bleed - ((PK_own->level * 0.4 + 2) * PK_own->skill_3 * PK_own->attack / (PK_enemy->defence * 50.0) + 2);  //伤害值计算//
+						}
+						if (x == 190 && y == 365 + 60)
+						{
+							LOGFONT f;
+							gettextstyle(&f);                     // 获取当前字体设置
+							f.lfHeight = 25;                      // 设置字体高度
+							wcscpy_s(f.lfFaceName, _T("楷体"));    // 设置字体
+							setbkmode(TRANSPARENT);
+							settextcolor(RED);
+							settextstyle(&f);                      // 设置字体样式
+							outtextxy(370, 360, Name);
+							outtextxy(480, 360, _T("!"));
+							outtextxy(380, 400, _T("使用打雷绝招"));
+							FlushBatchDraw();							  //结束批量绘制
+							Sleep(500);
+							thunder();
+							PK_enemy->bleed = PK_enemy->bleed - ((PK_own->level * 0.4 + 2) * PK_own->skill_3 * PK_own->attack / (PK_enemy->defence * 50.0) + 2);  //伤害值计算//
+						}
+						if (PK_enemy->bleed >= 0)
+						{
+							bleed_Width2 = PK_enemy->bleed * 1.0 / fullbleed2 * 125;
+						}
+						else
+						{
+							bleed_Width2 = 0 / fullbleed2 * 125;
+						}
+						turn = 0;
+
+						putimage(350, 0, &imbkk);
+						jump2 = 15;
+						load_PK_picture(PK_enemy);
+						FlushBatchDraw();
+						Sleep(150);
+						putimage(350, 0, &imbkk);
+						jump2 = 20;
+						load_PK_picture(PK_enemy);
+						FlushBatchDraw();
+						Sleep(150);
+						putimage(350, 0, &imbkk);
+						jump2 = 15;
+						load_PK_picture(PK_enemy);
+						FlushBatchDraw();
+						Sleep(150);
+						putimage(350, 0, &imbkk);
+						jump2 = 20;
+						load_PK_picture(PK_enemy);
+						FlushBatchDraw();
+						Sleep(150);
+						putimage(350, 0, &imbkk);
+						jump2 = 15;
+						load_PK_picture(PK_enemy);
+						FlushBatchDraw();
+						Sleep(500);
+
+					}
+					if (input == 'x' || input == 'X')
+						choose = 1;
+				}
+			}
+			FlushBatchDraw();							  //结束批量绘制
+		}
+	}
+}
+//战斗函数
+void fight()
+{
+	int i;
+	for (i = 0; i < pokemon_number; i++)				 //遍历精灵坐标数组，
+	{
+		if (abs(map_init_x + character_x - PK[i].x) <= 15 && abs(map_init_y + character_y - PK[i].y) <= 15 && PK[i].judge == 0)
+		{
+			interface_change_animation();		    //界面切换动画
+			fight_show(&PK[i], &PK[0]);				 //战斗显示函数	
+			interface_change_animation();       //界面切换动画
+			startup_pokemon_bleed();           //重置精灵血量
+		}
+	}
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //剧情部分
@@ -1075,11 +1586,17 @@ void plot_1()
 void main()
 {
 	startup_music();
+	startup_pokemon_judge();					  //对精灵时间属性进行初始化
+	startup_pokemon();						     //对精灵基础属性进行初始化
+	startup_pokemon_bleed();                   //对精灵血量进行初始化
+	startup_pokemon_destination();		      //对精灵位置进行初始化
 	startup_map_show();                             //对地图进行初始化
 	start_menu();                                  //登陆界面
 	while (1)
 	{
+		pokemon_refresh();                            //精灵刷新
 		map_show();										//地图显示函数
+		fight();												//战斗函数
 		operate();								// 客户交互函数
 	}
 }
